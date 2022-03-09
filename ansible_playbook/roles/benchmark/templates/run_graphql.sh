8832ac6b727a834a43ad0558646d3cb8997f0b6f
nkaralis <nkaralis@mail.uni-paderborn.de>
2022-03-09 09:29:32 +0100

    updated run scripts

--- ansible_playbook/roles/benchmark/templates/run_graphql.sh
+++ ansible_playbook/roles/benchmark/templates/run_graphql.sh
@@ -4,6 +4,7 @@ echo "$(date): GraphQL benchmarks"
 # run Query Mixes suites -- only for 1 worker
 echo "$(date): Running Query Mixes suites (one worker only)"
 # run neo4j
+echo 3 >/proc/sys/vm/drop_caches
 echo "$(date): Starting Neo4J for LinGBM1000 (qts), QM-Workers: 1"
 ./iguana_suites/graphql/neo4j/lingbm1000/1-start.sh &> /dev/null
 echo "$(date): Running IGUANA (QM Limit) for Neo4J and LinGBM1000 (qts) , QM-Workers: 1"
@@ -11,6 +12,7 @@ echo "$(date): Running IGUANA (QM Limit) for Neo4J and LinGBM1000 (qts) , QM-Wor
 echo "$(date): Stopping Neo4J for LinGBM1000 (qts), QM-Workers: 1"
 ./iguana_suites/graphql/neo4j/lingbm1000/stop.sh &> /dev/null
 # run tentrislj
+echo 3 >/proc/sys/vm/drop_caches
 echo "$(date): Starting Tentris for LinGBM1000 (qts), QM-Workers: 1"
 ./iguana_suites/graphql/TentrisLJ/lingbm1000/1-start.sh &> /dev/null
 echo "$(date): Running IGUANA (QM Limit) for Tentris and LinGBM1000 (qts), QM-Workers: 1"
@@ -18,6 +20,7 @@ echo "$(date): Running IGUANA (QM Limit) for Tentris and LinGBM1000 (qts), QM-Wo
 echo "$(date): Stopping Tentris for LinGBM1000 (qts), QM-Workers: 1"
 ./iguana_suites/graphql/TentrisLJ/stop.sh &> /dev/null
 # run tentrisljbase
+echo 3 >/proc/sys/vm/drop_caches
 echo "$(date): Starting Tentris for LinGBM1000 (qts), QM-Workers: 1"
 ./iguana_suites/graphql/TentrisLJBase/lingbm1000/1-start.sh &> /dev/null
 echo "$(date): Running IGUANA (QM Limit) for Tentris and LinGBM1000 (qts), QM-Workers: 1"
@@ -29,6 +32,7 @@ echo "$(date): Running Time Limit suites (multiple workers)"
 for i in "${workers[@]}"
 do
 	# run neo4j
+	echo 3 >/proc/sys/vm/drop_caches
 	echo "$(date): Starting Neo4J (all), T-Workers: $i"
 	./iguana_suites/graphql/neo4j/lingbm1000/"$i"-start.sh &> /dev/null
 	echo "$(date): Running IGUANA (T Limit) for Neo4J (all) , T-Workers: $i"
@@ -36,6 +40,7 @@ do
 	echo "$(date): Stopping Neo4J (all), QM-Workers: $i"
 	./iguana_suites/graphql/neo4j/lingbm1000/stop.sh &> /dev/null
 	# run tentrislj
+	echo 3 >/proc/sys/vm/drop_caches
 	echo "$(date): Starting TentrisLJ (all), T-Workers: $i"
 	./iguana_suites/graphql/TentrisLJ/lingbm1000/"$i"-start.sh &> /dev/null
 	echo "$(date): Running IGUANA (T Limit) for TentrisLJ (all), T-Workers: $i"
@@ -43,6 +48,7 @@ do
 	echo "$(date): Stopping Tentris (all), T-Workers: $i"
 	./iguana_suites/graphql/TentrisLJ/stop.sh &> /dev/null
 	# run tentrisljbase
+	echo 3 >/proc/sys/vm/drop_caches
 	echo "$(date): Starting TentrisLJ (all), T-Workers: $i"
 	./iguana_suites/graphql/TentrisLJBase/lingbm1000/"$i"-start.sh &> /dev/null
 	echo "$(date): Running IGUANA (T Limit) for TentrisLJ (all), T-Workers: $i"
--- ansible_playbook/roles/benchmark/templates/run_sparql.sh
+++ ansible_playbook/roles/benchmark/templates/run_sparql.sh
@@ -2,17 +2,29 @@
 triplestores=( "Fuseki" "Blazegraph" "GraphDB" "Virtuoso" "TentrisLJ" )
 workers=( 1 4 8 16 )
 echo "SPARQL benchmark"
+echo "Running Query Mixes suites (one worker)"
+for ts in "${triplestores[@]}"
+do
+	echo 3 >/proc/sys/vm/drop_caches
+	echo "$(date): Starting $ts"
+	./iguana_suites/sparql/"$ts"/watdiv10000/1-start.sh &> /dev/null
+	echo "$(date): Running IGUANA (Query Mixes) for $ts"
+	./iguana-run.sh ./iguana_suites/sparql/"$ts"/watdiv10000/1-QM.yml &> /dev/null
+	echo "$(date): Stopping $ts"
+	./iguana_suites/sparql/"$ts"/stop.sh &> /dev/null
+done 
 echo "Running Time Limit suites (multiple workers)"
 for ts in "${triplestores[@]}"
 do
 	for i in "${workers[@]}"
 	do
-		if [[ "$ts" == "GraphDB" ]] && ((workers > 1)) ; then # graphdb-free does not support concurrent clients
+		if [ "$ts" == "GraphDB" ] && [ "$i" -gt "1" ] ; then # graphdb-free does not support concurrent clients
     	continue
   	fi
+		echo 3 >/proc/sys/vm/drop_caches
 		echo "$(date): Starting $ts, Workers: $i"
 		./iguana_suites/sparql/"$ts"/watdiv10000/"$i"-start.sh &> /dev/null
-		echo "$(date): Running IGUANA (QM Limit) for $ts, Workers: $i"
+		echo "$(date): Running IGUANA (Time Limit) for $ts, Workers: $i"
 		./iguana-run.sh ./iguana_suites/sparql/"$ts"/watdiv10000/"$i"-T.yml &> /dev/null
 		echo "$(date): Stopping $ts, Workers: $i"
 		./iguana_suites/sparql/"$ts"/stop.sh &> /dev/null
